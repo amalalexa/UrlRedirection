1291074ef4173b9d42238dd495f82a0df0e07915
AmlAlx007 <amlalx007@gmail.com>
2021-08-25 10:06:27 +0100

    Initial spring app

---
+++ url/src/test/java/com/redirection/url/UrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redirection.url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
